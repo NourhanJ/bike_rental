94e82e5b167d960849a44466aa3f29a65613e535
Karim Press <web.development@karimpress.com>
2023-05-16 15:24:08 +0300

    requests update

--- request.php
+++ request.php
@@ -28,7 +28,7 @@ $h_req = array();
 
 //get all requests from database
 // if($temp = @mysqli_query($conn, "CALL SelectAllRequestProcedure('".$_COOKIE['CR-userID']."')")){
-  if($temp = @mysqli_query($conn, "SELECT * FROM request, bike, bike_accessories, users WHERE (request.id_user = '".$_COOKIE['CR-userID']."' AND bike.id_bike = request.id_bike) OR (request.id_user = '".$_COOKIE['CR-userID']."' AND bike_accessories.id_accessory = request.id_accessory) GROUP BY request.id_request;")){
+  if($temp = @mysqli_query($conn, "SELECT * FROM request LEFT JOIN bike ON bike.id_bike = request.id_bike LEFT JOIN bike_accessories ON bike_accessories.id_accessory = request.id_accessory JOIN users ON request.id_user = users.id_user WHERE request.id_user = '".$_COOKIE['CR-userID']."' GROUP BY request.id_request;")){
   while($result = @mysqli_fetch_assoc($temp))
     if ($result != null) {
       $req_count[0]++;
